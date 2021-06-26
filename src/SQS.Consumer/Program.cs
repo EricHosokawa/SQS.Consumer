9ab7f684a7efba5e4afa2646c3fb2efc5ed72468
EricHosokawa <erichosokawa@gmail.com>
2021-06-26 14:21:21 -0300

    commit solution

---
+++ src/SQS.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace SQS.Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("My AWS SQS Consumer!");
+
+            try
+            {
+                var client = new AmazonSQSClient(RegionEndpoint.USEast2);
+                var url = "https://sqs.us-east-2.amazonaws.com/713434528261/teste_v1";
+
+                var request = new ReceiveMessageRequest
+                {
+                    QueueUrl = url
+                };
+
+                while (true) 
+                {
+                    var response = await client.ReceiveMessageAsync(request);
+
+                    foreach (var message in response.Messages)
+                    {
+                        await client.DeleteMessageAsync(url, message.ReceiptHandle);
+                        Console.WriteLine($"Message received and removed from SQS: {message.Body}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+
+                throw ex;
+            }
+        }
+    }
+}
